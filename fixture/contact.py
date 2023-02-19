19fffdd7ec4114e03f943d0cbedd058446362c29
stbelaya <stbelaya@gmail.com>
2023-02-19 10:58:26 +0300

    Убрала заполнение фото из теста

--- fixture/contact.py
+++ fixture/contact.py
@@ -20,7 +20,8 @@ class ContactHelper:
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
         wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("photo").send_keys(contact.photo)
+        if contact.photo is not None:
+            wd.find_element_by_name("photo").send_keys(contact.photo)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
         wd.find_element_by_name("title").send_keys(contact.title)
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,7 +6,7 @@ def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Svetlana", middlename="Borisovna", lastname="Kovaleva",
                        nickname="Belaya",
-                       photo="C:\\Sync\\YandexDisk\\FoldersForWindows\\Pictures\\LogoLP.jpg",
+                       photo=None,
                        title="Tester", company="The Best",
                        address="Lenina, 8", mobile="89001001213", home="555555", work="121212",
                        fax="111111", email="stbelaya@gmail.com", email2="stbelaya2@gmail.com",
