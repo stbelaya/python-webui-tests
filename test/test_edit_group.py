5405877aa76a3c78c517e7295163c91d776dd401
stbelaya <stbelaya@gmail.com>
2023-02-24 18:37:54 +0300

    Добавлено предусловие проверки существования групп в тестах модификации группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,12 +3,18 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first(Group(name="Updated name", header="Updated header", footer="Updated footer"))
 
 
 def test_edit_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first(Group(name="Updated name2"))
 
 
 def test_edit_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first(Group(header="Updated header3"))
