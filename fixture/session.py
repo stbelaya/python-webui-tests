30bdd6f13397ab7af902030f8d6c4f2bd55a8dd8
stbelaya <stbelaya@gmail.com>
2023-02-25 12:10:41 +0300

    Удалена задержка 5 сек, удален поиск элемента из метода logout

--- fixture/session.py
+++ fixture/session.py
@@ -25,7 +25,6 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user")
 
     def ensure_logout(self):
         wd = self.app.wd
