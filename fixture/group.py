f97cb37798ab6df28a29238bc23c82a0853ea059
stbelaya <stbelaya@gmail.com>
2023-02-25 16:55:02 +0300

    Логика проверки на то, открыта ли страница вынесена в отдельный метод is_on_page() в классе NavigationHelper, добавлена проверка на присутствие диалогового окна

--- fixture/group.py
+++ fixture/group.py
@@ -3,14 +3,6 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def open_groups_page(self):
-        wd = self.app.wd
-        self.app.navigation.navigate_to_group_page("groups")
-
-    def return_to_groups_page(self):
-        wd = self.app.wd
-        self.app.navigation.navigate_to_group_page("group page")
-
     def fill_form(self, group):
         wd = self.app.wd
         self.change_field_value("group_name", group.name)
@@ -26,26 +18,26 @@ class GroupHelper:
 
     def create(self, group):
         wd = self.app.wd
-        self.open_groups_page()
+        self.app.navigation.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         self.fill_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
+        self.app.navigation.return_to_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
-        self.open_groups_page()
+        self.app.navigation.open_groups_page()
         self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
+        self.app.navigation.return_to_groups_page()
 
     def edit_first(self, group):
         wd = self.app.wd
-        self.open_groups_page()
+        self.app.navigation.open_groups_page()
         self.select_first_group()
         # submit edition
         wd.find_element_by_name("edit").click()
@@ -53,7 +45,7 @@ class GroupHelper:
         self.fill_form(group)
         # submit group creation
         wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
+        self.app.navigation.return_to_groups_page()
 
     def select_first_group(self):
         wd = self.app.wd
@@ -61,5 +53,5 @@ class GroupHelper:
 
     def count(self):
         wd = self.app.wd
-        self.open_groups_page()
+        self.app.navigation.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
--- fixture/navigation.py
+++ fixture/navigation.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.support import expected_conditions as ec
+
+
 class NavigationHelper:
 
     def __init__(self, app):
@@ -5,13 +8,26 @@ class NavigationHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.get("https://localhost/addressbook/")
+        if self.is_on_page("localhost/addressbook/", "searchstring") is False or ec.alert_is_present() is True:
+            wd.get("https://localhost/addressbook/")
 
     def return_to_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home page").click()
+        if ec.alert_is_present() is True:
+            wd.switch_to.alert.dismiss()
+        elif self.is_on_page("localhost/addressbook/", "searchstring") is False:
+            wd.find_element_by_link_text("home page").click()
 
-    def navigate_to_group_page(self, text):
+    def is_on_page(self, url, element):
         wd = self.app.wd
-        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
-            wd.find_element_by_link_text(text).click()
+        return wd.current_url.endswith(url) and len(wd.find_elements_by_name(element)) > 0
+
+    def open_groups_page(self):
+        wd = self.app.wd
+        if self.is_on_page("/group.php", "new") is False:
+            wd.find_element_by_link_text("groups").click()
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        if self.is_on_page("/group.php", "new") is False:
+            wd.find_element_by_link_text("group page").click()
