5f529c6129e2e2027e8d0b21f0d663cbcd3c0f69
stbelaya <stbelaya@gmail.com>
2023-03-03 12:51:47 +0300

    Добавлен ассерт на то, что длина списка групп не изменилась в тесты редактирования групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,16 +5,25 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first(Group(name="Updated name", header="Updated header", footer="Updated footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first(Group(name="Updated name2"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first(Group(header="Updated header3"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
