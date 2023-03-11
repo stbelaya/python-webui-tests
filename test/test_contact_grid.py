5432646ee38a26acac4fff3055832d345158fa2b
stbelaya <stbelaya@gmail.com>
2023-03-11 12:11:37 +0300

    Тестовые данные для теста добавления групп вынесены вне теста, использована параметризация, добавлена случайная генерация строк - полей группы В тесте сравнения контактов в гриде генерация случайных строк вынесена в отдельную функцию.

--- test/test_contact_grid.py
+++ test/test_contact_grid.py
@@ -2,21 +2,28 @@ from random import randrange, choice
 import string
 from model.contact import Contact
 from fixture.contact import merge_emails_like_on_home_page, merge_phones_like_on_home_page
+import random
+import string
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 def test_contact_grid_on_home_page(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(
-            firstname="".join([choice(string.ascii_letters + string.digits) for i in range(randrange(20))]),
-            lastname="".join([choice(string.ascii_letters + string.digits) for i in range(randrange(20))]),
-            address="".join([choice(string.ascii_letters + string.digits + " .-") for i in range(randrange(50))]),
-            email="".join([choice(string.ascii_letters + string.digits) for i in range(randrange(20))]),
-            email2="".join([choice(string.ascii_letters + string.digits) for i in range(randrange(20))]),
-            email3="".join([choice(string.ascii_letters + string.digits) for i in range(randrange(20))]),
-            home="".join([choice(string.digits + "+ - ()") for i in range(randrange(15))]),
-            mobile="".join([choice(string.digits + "+ - ()") for i in range(randrange(15))]),
-            work="".join([choice(string.digits + "+ - ()") for i in range(randrange(15))]),
-            phone2="".join([choice(string.digits + "+ - ()") for i in range(randrange(15))])
+            firstname=random_string("name", 15),
+            lastname=random_string("lastname", 15),
+            address=random_string("address", 50),
+            email=random_string("email", 15),
+            email2=random_string("email2", 15),
+            email3=random_string("email3", 15),
+            home=random_string("home", 10),
+            mobile=random_string("mobile", 10),
+            work=random_string("work", 10),
+            phone2=random_string("phone2", 10)
         ))
     all_contacts = app.contact.get_contact_list()
     index = randrange(len(all_contacts))
