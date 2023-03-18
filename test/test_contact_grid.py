53c252a1b116a973303f5f3cb4b051ab2d62858e
stbelaya <stbelaya@gmail.com>
2023-03-18 12:10:55 +0300

    Добавлено использование тестовых данных из json файла в тесте отображения полей контакта в гриде (для создания контакта, если нет ни одного)

--- test/test_contact_grid.py
+++ test/test_contact_grid.py
@@ -4,20 +4,9 @@ from model.contact import Contact
 from fixture.contact import merge_emails_like_on_home_page, merge_phones_like_on_home_page
 
 
-def test_contact_grid_on_home_page(app):
+def test_contact_grid_on_home_page(app, json_contacts):
     if app.contact.count() == 0:
-        app.contact.create(Contact(
-            firstname=random_string("name", 15),
-            lastname=random_string("lastname", 15),
-            address=random_string("address", 50),
-            email=random_string("email", 15),
-            email2=random_string("email2", 15),
-            email3=random_string("email3", 15),
-            home=random_string("home", 10),
-            mobile=random_string("mobile", 10),
-            work=random_string("work", 10),
-            phone2=random_string("phone2", 10)
-        ))
+        app.contact.create(json_contacts)
     all_contacts = app.contact.get_contact_list()
     index = randrange(len(all_contacts))
     contact_from_home_page = app.contact.get_contact_list()[index]
