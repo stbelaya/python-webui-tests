f1ec90a2433dd38354e1d958a811604948cc33b6
stbelaya <stbelaya@gmail.com>
2023-03-24 16:48:35 +0300

    Обновлён тест удаления групп, теперь группа удаляется по id, добавлены новые методы выбора и удаления группы по id

--- fixture/group.py
+++ fixture/group.py
@@ -44,6 +44,15 @@ class GroupHelper:
         self.app.navigation.return_to_groups_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.app.navigation.open_groups_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.app.navigation.return_to_groups_page()
+        self.group_cache = None
+
     def edit_first(self, group):
         self.edit_group_by_index(0, group)
 
@@ -67,6 +76,10 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector(f"input[value='{id}']").click()
 
     def count(self):
         wd = self.app.wd
