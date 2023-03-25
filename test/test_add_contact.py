5a761ef11b4cbd066d1e9bab3ca3b4f5512c49bd
stbelaya <stbelaya@gmail.com>
2023-03-25 18:38:51 +0300

    Тест добавления контакта переделан на получение списка контактов из БД. Добавлена проверка по параметру сравнения списка из БД со списком с UI.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,13 @@ from model.contact import Contact
 from fixture.contact import clear_contact
 
 
-def test_add_contact(app, json_contacts):
+def test_add_contact(app, json_contacts, db, check_ui):
     contact = json_contacts
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     app.contact.create(contact)
-    assert app.contact.count() == len(old_contacts) + 1
-    new_contacts = app.contact.get_contact_list()
-    old_contacts.append(clear_contact(contact))
+    new_contacts = db.get_contact_list()
+    old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
+    if check_ui:
+        assert sorted(map(clear_contact, new_contacts), key=Contact.id_or_max) == \
+               sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
