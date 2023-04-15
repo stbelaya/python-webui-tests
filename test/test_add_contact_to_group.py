b4567ac96ba12578ce323266094ce03ff4a631b1
stbelaya <stbelaya@gmail.com>
2023-04-15 19:21:15 +0300

    add allure.steps for test_add_contact_to_group

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import random
+import allure
 
 from model.contact import Contact
 from fixture.processing import clear_contact_to_db
@@ -7,15 +8,21 @@ from model.group import Group
 
 
 def test_add_contact_to_group(app, db):
-    groups = db.get_group_list()
-    if not groups:
-        app.group.create(Group(name="group test"))
+    with allure.step("Given a non-empty group list"):
         groups = db.get_group_list()
-    group = random.choice(groups)
-    contacts = db.get_contacts_not_in_group(group)
-    if not contacts:
-        app.contact.create(Contact(firstname="First test1", lastname="Last test1"))
+        if not groups:
+            app.group.create(Group(name="group test"))
+            groups = db.get_group_list()
+    with allure.step("Given a random group from the list"):
+        group = random.choice(groups)
+    with allure.step("Given a non-empty contact list for contacts without groups"):
         contacts = db.get_contacts_not_in_group(group)
-    contact = random.choice(contacts)
-    app.contact.add_contact_to_group(contact.id, group.id)
-    assert clear_contact_to_db(contact) in db.get_contacts_in_group(group)
+        if not contacts:
+            app.contact.create(Contact(firstname="First test1", lastname="Last test1"))
+            contacts = db.get_contacts_not_in_group(group)
+    with allure.step("Given a random contact from the list"):
+        contact = random.choice(contacts)
+    with allure.step(f"When I add the contact {contact} to the group {group}"):
+        app.contact.add_contact_to_group(contact.id, group.id)
+    with allure.step("Then the contact is in the group"):
+        assert clear_contact_to_db(contact) in db.get_contacts_in_group(group)
