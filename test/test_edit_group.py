c191eff068463bc8e03be47c9e43ff7e27716231
stbelaya <stbelaya@gmail.com>
2023-04-15 18:55:06 +0300

    add allure.steps for test_edit_group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 import random
+import allure
 
 from fixture.processing import propagate, clear_group
 from model.group import Group
 
 
 def test_edit_some_group(app, json_groups, db, check_ui):
-    if len(db.get_group_list()) == 0:
-        app.group.create(Group(name="test"))
-    old_groups = db.get_group_list()
-    group = random.choice(old_groups)
+    with allure.step("Given a non-empty group list"):
+        if not db.get_group_list():
+            app.group.create(Group(name="test"))
+        old_groups = db.get_group_list()
+    with allure.step("Given a random group from the list"):
+        group = random.choice(old_groups)
     new_group = json_groups
     new_group.id = group.id
-    app.group.edit_group_by_id(group.id, new_group)
-    new_groups = db.get_group_list()
-    old_groups.remove(group)
-    old_groups.append(propagate(group, new_group))
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(map(clear_group, new_groups), key=Group.id_or_max) == \
-               sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with allure.step(f"When I edit the group {group} in the list according to given group {new_group}"):
+        app.group.edit_group_by_id(group.id, new_group)
+    with allure.step("Then the new group list is equal to the old list with the edited group"):
+        new_groups = db.get_group_list()
+        old_groups.remove(group)
+        old_groups.append(propagate(group, new_group))
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(map(clear_group, new_groups), key=Group.id_or_max) == \
+                   sorted(app.group.get_group_list(), key=Group.id_or_max)
