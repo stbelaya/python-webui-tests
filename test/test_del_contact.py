1895172decec7a051f5c5d1586cda8085fc47ea2
stbelaya <stbelaya@gmail.com>
2023-04-15 19:13:10 +0300

    add allure.steps for test_edit_contact, test_del_contact

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,19 +1,24 @@
 import random
+import allure
 
 from fixture.processing import clear_contact
 from model.contact import Contact
 
 
 def test_delete_some_contact(app, db, check_ui):
-    if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(firstname="First_test", lastname="Last_test"))
-    old_contacts = db.get_contact_list()
-    contact = random.choice(old_contacts)
-    app.contact.delete_contact_by_id(contact.id)
-    assert len(old_contacts) - 1 == app.contact.count()
-    new_contacts = db.get_contact_list()
-    old_contacts.remove(contact)
-    assert old_contacts == new_contacts
-    if check_ui:
-        assert sorted(map(clear_contact, new_contacts), key=Contact.id_or_max) \
-               == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    with allure.step("Given a non-empty contact list"):
+        if not db.get_contact_list():
+            app.contact.create(Contact(firstname="First_test", lastname="Last_test"))
+        old_contacts = db.get_contact_list()
+    with allure.step("Given a random contact from the list"):
+        contact = random.choice(old_contacts)
+    with allure.step(f"When I delete the contact {contact} from the list"):
+        app.contact.delete_contact_by_id(contact.id)
+    with allure.step("Then the new contact list is equal to the old list without the deleted contact"):
+        assert len(old_contacts) - 1 == app.contact.count()
+        new_contacts = db.get_contact_list()
+        old_contacts.remove(contact)
+        assert old_contacts == new_contacts
+        if check_ui:
+            assert sorted(map(clear_contact, new_contacts), key=Contact.id_or_max) \
+                   == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 import random
+import allure
 
 from model.contact import Contact
 from fixture.processing import propagate, clear_contact
 
 
 def test_edit_some_contact(app, json_contacts, db, check_ui):
-    if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(firstname="test"))
-    old_contacts = db.get_contact_list()
-    contact = random.choice(old_contacts)
+    with allure.step("Given a non-empty contact list"):
+        if not db.get_contact_list():
+            app.contact.create(Contact(firstname="test"))
+        old_contacts = db.get_contact_list()
+    with allure.step("Given a random contact from the list"):
+        contact = random.choice(old_contacts)
     new_contact = json_contacts
     new_contact.id = contact.id
-    app.contact.edit_contact_by_id(contact.id, new_contact)
-    new_contacts = db.get_contact_list()
-    old_contacts.remove(contact)
-    old_contacts.append(propagate(contact, new_contact))
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-    if check_ui:
-        assert sorted(map(clear_contact, new_contacts), key=Contact.id_or_max) \
-               == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    with allure.step(f"When I edit the contact {contact} from the list according to given contact {new_contact}"):
+        app.contact.edit_contact_by_id(contact.id, new_contact)
+    with allure.step("Then the new contact list is equal to the old list with the edited contact"):
+        new_contacts = db.get_contact_list()
+        old_contacts.remove(contact)
+        old_contacts.append(propagate(contact, new_contact))
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+        if check_ui:
+            assert sorted(map(clear_contact, new_contacts), key=Contact.id_or_max) \
+                   == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
