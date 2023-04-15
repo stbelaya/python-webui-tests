30a8348a33998b5420e51c86bda359f353740622
stbelaya <stbelaya@gmail.com>
2023-04-15 16:16:56 +0300

    Regenerated test-data for groups and contacts (commented replacing "'<\\"

--- generator/generation_helper.py
+++ generator/generation_helper.py
@@ -4,15 +4,15 @@ import calendar
 
 
 def random_string(prefix, maxlen):
-    symbols = (string.ascii_letters + string.digits + " "*10 + string.punctuation).\
-        replace("'", "").replace("\\", "").replace("<", "")
+    symbols = (string.ascii_letters + string.digits + " "*10 + string.punctuation) #.\
+        # replace("'", "").replace("\\", "").replace("<", "")
     # временно убираю проблемные символы, чтобы тесты не падали на known issues
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 def random_text(prefix, maxlen, maxrows):
-    symbols = (string.ascii_letters + string.digits + " "*10 + "\n" * random.randrange(maxrows) + string.punctuation).\
-        replace("'", "").replace("\\", "").replace("<", "")
+    symbols = (string.ascii_letters + string.digits + " "*10 + "\n" * random.randrange(maxrows) + string.punctuation) # .\
+        # replace("'", "").replace("\\", "").replace("<", "")
     # временно убираю проблемные символы, чтобы тесты не падали на known issues
     return prefix + "".join([random.choice(symbols.replace("'", "")) for i in range(random.randrange(maxlen))])
 
